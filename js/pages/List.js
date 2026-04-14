e66ee6f03462461bb59948f608bb45c0a5abdb3d
Kriskokos228 <92459068+Kriskokos228@users.noreply.github.com>
2026-04-14 04:26:10 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -55,8 +55,8 @@ export default {
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
                     </ul>
+                    <h2>Records</h2>
                     <table class="skibidi">
-                        <h2>Records</h2>
                         <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
                         <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
                         <p v-else>This level does not accept new records.</p>
